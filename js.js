95a0bb95bcc6078e7e529c3e77b6b487c9d4b9c6
max Bertfield <mbertfield@stuy.edu>
2017-02-13 22:54:35 -0500

    better

--- js.js
+++ js.js
@@ -2,7 +2,7 @@ var c = document.getElementById("can");
 var ctx = c.getContext('2d');
 ctx.fillStyle = '#C0FFEE';
 var createRect = function(e){
-    ctx.fillRect(e.clientX - 100, e.clientY - 100, 50, 50);
+    ctx.fillRect(e.clientX, e.clientY, 50, 50);
     console.log('he');
 };
 c.addEventListener('click', createRect);
